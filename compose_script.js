6e0252fbafc9b50fbd28b2dab8bf693df2ceea23
Lionel Holt <lionel@treetix.com>
2022-10-23 23:32:19 -0700

    Don't overwrite existing style

--- compose_script.js
+++ compose_script.js
@@ -10,9 +10,13 @@ let observer = new MutationObserver(function(mutations) {
       for (let target of mutation.addedNodes) {
 				if (target.nodeName == "IMG") {
 					try {
-						target.setAttribute("style", "max-width:100%");
-						target.removeAttribute("width");
-						target.removeAttribute("height");
+						let str = target.getAttribute("style") || ""; // clears null
+						if (!str.includes("max-width:")) {
+							if (str) str += " ";
+							target.setAttribute("style", `${str}max-width: 100%;`);
+							target.removeAttribute("width");
+							target.removeAttribute("height");
+						}
 					}
 					catch (err) {
 						console.log(err);
